7167d01b1788144f664ba70b745a78f36d415848
dima <d.ananyev@a1qa.com>
2020-10-05 14:25:20 +0400

    feat: push solution for Towel Sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let correctOrderArray = []
+  let resultArray = []
+  if (!Array.isArray(matrix) || matrix.length == 0) return []
+  if (matrix.length == 1) return matrix
+  if (matrix.length > 1) {
+      matrix.forEach( (item, index) => {
+        if ((index + 1)%2 == 0) {
+          correctOrderArray.push(item.slice().reverse())
+      } else {
+        correctOrderArray.push(item)
+      }
+    })
+  }
+  correctOrderArray.forEach((item) => {
+    item.forEach((i) => {
+      resultArray.push(i)
+    })
+  })
+  return resultArray
 }
